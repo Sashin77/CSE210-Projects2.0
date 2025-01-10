503017af982cfff06a738823ec108a7e32c1f533
Nephites7 <sashinshim27@gmail.com>
2025-01-10 15:52:23 -0700

    Completed C# Prep 2 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please tell us your first name! ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Please tell us your last name! ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Ah! Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your or the student's grade percentage: ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        string grade = "";
+        string sign = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        if (grade != "F")
+        {
+            int lastDigit = percentage % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (grade == "A" && sign == "+")
+        {
+            sign = ""; 
+        }
+        if (grade == "F")
+        {
+            sign = ""; 
+        }
+
+        Console.WriteLine($"The grade is: {grade}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Success, this class was passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, the class was not passed, with this grade.");
+        }
     }
 }
