cc19b6589f39e473b238ec4befa8249fff59a834
Nephites7 <sashinshim27@gmail.com>
2025-01-23 15:03:06 -0700

    Abstraction, hopefully I do get these jobs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Job job1 = new Job();
+        job1._company = "BYU";
+        job1._jobTitle = "Computer Engineeer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Excel";
+        job2._jobTitle = "Accountant";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Sashin Shim";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
